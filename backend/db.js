5cf56161e07d5131d0c719c08ff89de4f37f8655
kchethan <kchethan@falcasolutions.com>
2024-09-12 22:59:52 +0530

    MongoDB Connection

---
+++ backend/db.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const mongoURI = "mongodb://localhost:27017/analytics"
+
+const connectToMongo = async () => {
+    try {
+        await mongoose.connect(mongoURI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true 
+        })
+        console.log("Connected to Mongo Successfully");
+    } catch (error) {
+        console.log("Failed to connect to MongoDB", error);
+    }
+};
+
+module.exports = connectToMongo
